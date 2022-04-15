e521236a90ccc068b2784c14b059434d6b1ff7f5
agustina-anitsuga <agustina.dagnino@gmail.com>
2022-04-15 16:27:23 -0300

    mirror composite flight plan

--- characters/flight-plans/composite-plans.lua
+++ characters/flight-plans/composite-plans.lua
@@ -10,21 +10,41 @@ function Demo1CompositeFlightPlan:new()
 end
 
 Demo2CompositeFlightPlan = CompositeFlightPlan:extend()
-function Demo2CompositeFlightPlan:new( mirrored )
+function Demo2CompositeFlightPlan:new( mirrored, hoverX, hoverY )
     local trajectory = {100,0, 200,80, 350,100, 500,250, 1000,400, 500,500, 350,400, 350,100}
+    local count = table.getn( trajectory )
+    local gotoPlan, hoverPlan
+    local bezierEndX = trajectory[count-1]
+    local bezierEndY = trajectory[count]
     if mirrored then
+        local screenWidth = love.graphics.getWidth()
         trajectory = mirrorVertically(trajectory)
+        gotoPlan = GoToCoordinateFlightPlan(screenWidth-bezierEndX,bezierEndY,screenWidth-hoverX,hoverY)
+        hoverPlan = HorizontalHoverFlightPlan(screenWidth-hoverX,hoverY)
+    else
+        gotoPlan = GoToCoordinateFlightPlan(bezierEndX,bezierEndY,hoverX,hoverY)
+        hoverPlan = HorizontalHoverFlightPlan(hoverX,hoverY)
     end
     local bezierPlan = BezierFlightPlan(love.math.newBezierCurve(trajectory),0)
-    local gotoPlan = GoToCoordinateFlightPlan(350,100,100,500)
-    local hoverPlan = HorizontalHoverFlightPlan(100,500)
+        
     local plans = {}
     table.insert(plans,bezierPlan)
     table.insert(plans,gotoPlan)    
     table.insert(plans,hoverPlan)
+
     Demo1CompositeFlightPlan.super.new(self,plans)
 end
 
 function mirrorVertically( points )
-    return points
+    local len = table.getn( points )
+    local mirroredPoints = { n = len }
+    local screenWidth = love.graphics.getWidth()
+    for i=1,len do
+        if i%2 == 1 then
+            mirroredPoints[i] = screenWidth - points[i]
+        else
+            mirroredPoints[i] = points[i]
+        end 
+    end
+    return mirroredPoints
 end
